624ecb7547f734467014c9e32792cc89ce079b99
Gabriel Dantas <dantasbiell20@gmail.com>
2025-04-22 20:11:49 -0300

    update

--- src/settings/contents.js
+++ src/settings/contents.js
@@ -2,7 +2,7 @@ export const contents_about = [
     
     {
         title: "Boa Elaboração",
-        content: "Se você quiser executar a sua piscina de forma prática, o projeto vai te dar o passo a passo para fazer tudo corretamente. Com um processo prático e eficiente, garantindo qualidade e evitando sua dor de cabeça.",
+        content: "Se você quiser executar a sua piscina de forma prática, o projeto vai te dar o passo a passo para fazer tudo corretamente. Com um processo eficiente, garantindo qualidade e evitando sua dor de cabeça.",
         button: "Venha conhecer a M2 Projetos"
     },
 
